303b9a9c0c8634329abe826dfec01cd2eb8cc2d3
lrizza <leonardorizza@hotmail.com>
2023-08-28 12:11:32 -0300

    desafio 4 final

--- Desafio4/src/public/js/script.js
+++ Desafio4/src/public/js/script.js
@@ -1,54 +1,3 @@
-/* const realTimeProductsList = document.getElementById('realTimeProductsList');
-
-const socket = io();
-
-socket.emit('mensajeConexion', "Hola socket");
-
-socket.on('realtimeProducts', (products) => {
-    realTimeProductsList.innerHTML = '';
-
-    products.map((product) => {
-        const listItem = document.createElement('li');
-        listItem.innerHTML = `
-            ${product.title} - Precio: $${product.price}
-        `;
-        realTimeProductsList.appendChild(listItem);
-    });
-});
-
-const addProductForm = document.getElementById('addProductForm');
-
-addProductForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-
-    const title = document.getElementById('title').value;
-    const description = document.getElementById('description').value;
-    const status = document.getElementById('status').value;
-    const stock = parseInt(document.getElementById('stock').value);
-    const code = document.getElementById('code').value;
-    const thumbnail = document.getElementById('thumbnail').value;
-    const price = parseFloat(document.getElementById('price').value);
-
-    if (!title || !description || !status || isNaN(stock) || !code || !thumbnail || isNaN(price)) {
-        return;
-    }
-
-    const newProduct = {
-        title,
-        description,
-        status,
-        stock,
-        code,
-        thumbnail,
-        price,
-    };
-
-    socket.emit('addProduct', newProduct);
-    addProductForm.reset();
-});
-
- */
-
 const realTimeProductsList = document.getElementById('realTimeProductsList');
 const addProductForm = document.getElementById('addProductForm');
 const deleteProductForm = document.getElementById('deleteProductForm');
@@ -57,7 +6,6 @@ const socket = io();
 
 socket.emit('mensajeConexion', "Hola socket");
 
-// Función para actualizar la lista de productos
 function updateProductList(products) {
     realTimeProductsList.innerHTML = '';
 
@@ -69,7 +17,6 @@ function updateProductList(products) {
         realTimeProductsList.appendChild(listItem);
     });
 
-    // Agregar eventos click a los botones de eliminar
     const deleteButtons = document.querySelectorAll('.deleteButton');
     deleteButtons.forEach((button) => {
         button.addEventListener('click', (event) => {
@@ -79,12 +26,10 @@ function updateProductList(products) {
     });
 }
 
-// Escuchar los productos en tiempo real
 socket.on('realtimeProducts', (products) => {
     updateProductList(products);
 });
 
-// Escuchar el evento de envío de formulario para agregar producto
 addProductForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
@@ -114,7 +59,6 @@ addProductForm.addEventListener('submit', (event) => {
     addProductForm.reset();
 });
 
-// Escuchar el evento de envío de formulario para eliminar producto por ID
 deleteProductForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
